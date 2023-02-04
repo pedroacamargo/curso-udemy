0b2b36cf51ecc7077a170a07578c3ce152b4a1e6
pedroacamargo <pedroaugennes@gmail.com>
2023-02-04 23:53:45 +0000

    Laços ✅

---
+++ JavaScript/Atividades/LacosPratica/script.js
@@ -0,0 +1,14 @@
+let lista_frutas = []
+
+lista_frutas[0] = 'Banana'
+lista_frutas[1] = 'Maçã'
+lista_frutas[2] = 'Morango'
+lista_frutas[3] = 'Uva'
+
+let y = 0
+console.log(lista_frutas.length)
+
+while (y < lista_frutas.length) {
+    document.write(lista_frutas[y] + "<br>")
+    y++
+}
