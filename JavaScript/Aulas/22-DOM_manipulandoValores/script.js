775a39ee9e67bc5db59686707a7e184c485e7687
pedroacamargo <pedroaugennes@gmail.com>
2023-02-04 13:27:25 +0000

    Aulas ✅

--- JavaScript/Aulas/22-DOM_manipulandoValores/script.js
+++ JavaScript/Aulas/22-DOM_manipulandoValores/script.js
@@ -1,3 +1,30 @@
 function distribuiCaracter() {
-    document.getElementById("entrada")
+    let caracter = document.getElementById("entrada").value
+    // limpar o campo de digitação
+    document.getElementById("entrada").value = ""
+    console.log(caracter)
+
+    // limpar espaços em branco nas extremidades da string
+    caracter.trim()
+
+
+    switch(caracter) {
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+            // adicionar o caractere no campo referente a números
+            document.getElementById("numeros").value += caracter
+            break
+        default:
+            // adicionar o caractere no campo referente a letras
+            document.getElementById("letras").value += caracter
+    }
+
 }
---
+++ JavaScript/Aulas/23-DOM_manipulandoEstilos/script.js
@@ -0,0 +1,4 @@
+function modificaEstilo(corDeFundo) {
+    const elemento = document.getElementById("elemento")
+    elemento.style.backgroundColor = corDeFundo
+}
