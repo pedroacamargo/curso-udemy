7d0f1ebf3b0cbb066db9ebf0301a4089bffdead6
pedroacamargo <pedroaugennes@gmail.com>
2023-02-05 23:59:34 +0000

    audio ✅

--- JavaScript/Projeto/jogo.js
+++ JavaScript/Projeto/jogo.js
@@ -4,6 +4,8 @@ let vidas = 3
 let tempo = 60
 let criaMosquitoTempo = 1500
 
+const audioKill = new Audio("audio/morte2.mp3")
+
 let nivel = window.location.search
 nivel = nivel.replace('?', '')
 
@@ -59,9 +61,54 @@ function criarMosquito() {
     var posicaoX = Math.floor(Math.random() * largura) - 90
     var posicaoY = Math.floor(Math.random() * altura) - 90
 
+    let particula1X = posicaoX + 22
+    let particula1Y = posicaoY + 19
+    let particula2X = posicaoX + 30
+    let particula2Y = posicaoY + 30
+    let particula3X = posicaoX + 26
+    let particula3Y = posicaoY + 28
+    let particula4X = posicaoX + 34
+    let particula4Y = posicaoY + 15
+
+
     posicaoX = posicaoX < 0 ? 0 : posicaoX
     posicaoY = posicaoY < 0 ? 0 : posicaoY
 
+    let particula1 = document.createElement('div')
+    particula1.style.position = "absolute"
+    particula1.style.top = particula1Y + "px"
+    particula1.style.left = particula1X + "px"
+    particula1.style.width = "10px"
+    particula1.style.height = "10px"
+    particula1.style.backgroundColor = "red"
+    particula1.style.borderRadius = "100%"
+
+    let particula2 = document.createElement('div')
+    particula2.style.position = "absolute"
+    particula2.style.top = particula2Y + "px"
+    particula2.style.left = particula2X + "px"
+    particula2.style.width = "10px"
+    particula2.style.height = "10px"
+    particula2.style.backgroundColor = "red"
+    particula2.style.borderRadius = "100%"
+
+    let particula3 = document.createElement('div')
+    particula3.style.position = "absolute"
+    particula3.style.top = particula3Y + "px"
+    particula3.style.left = particula3X + "px"
+    particula3.style.width = "10px"
+    particula3.style.height = "10px"
+    particula3.style.backgroundColor = "red"
+    particula3.style.borderRadius = "100%"
+
+    let particula4 = document.createElement('div')
+    particula4.style.position = "absolute"
+    particula4.style.top = particula4Y + "px"
+    particula4.style.left = particula4X + "px"
+    particula4.style.width = "10px"
+    particula4.style.height = "10px"
+    particula4.style.backgroundColor = "red"
+    particula4.style.borderRadius = "100%"
 
     let mosquito = document.createElement('img')
     mosquito.src = "imagens/mosca.png" 
@@ -70,8 +117,24 @@ function criarMosquito() {
     mosquito.style.position = "absolute"
     mosquito.className = tamanhoAleatorio() + " " + ladoAleatorio()
     mosquito.id = 'mosquito'
+
+
     mosquito.onclick = function() {
+        // load serve para resetar o áudio
+        audioKill.load()
+        // play serve para dar play no áudio
+        audioKill.play()
+        // faz o mosquito sumir da tela
         this.remove()
+
+        // particulas quando o mosquito é morto
+        document.body.appendChild(particula1)
+        document.body.appendChild(particula2)
+        document.body.appendChild(particula3)
+        document.body.appendChild(particula4)
+
+
+
     }
     
     document.body.appendChild(mosquito)
